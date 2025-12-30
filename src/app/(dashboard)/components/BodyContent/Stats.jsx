ef0cfdb60b0754a762e8442d194beefabdfdc737
alaminislam34 <mi3548514@gmail.com>
2025-12-31 05:49:05 +0600

    added all components

--- src/app/(dashboard)/components/BodyContent/Stats.jsx
+++ src/app/(dashboard)/components/BodyContent/Stats.jsx
@@ -27,16 +27,15 @@ const Stats = () => {
       {stats.map((stat) => (
         <div
           key={stat.stat}
-          className={`flex flex-col gap-2 items-start rounded-2xl border-2 p-4 md:p-6`}
+          className={`flex flex-col gap-2 items-start rounded-2xl p-4 md:p-6`}
           style={{
-            borderColor: stat.color,
             backgroundColor: `${stat.color}10`, // 80 = 50% opacity in hex
           }}
         >
           <p style={{ color: stat.color }} className="lg:text-lg">
             {stat.stat}
           </p>
-          <h1 className="text-2xl md:text-3xl lg:text-4xl">{stat.count}</h1>
+          <h1 className="text-2xl md:text-3xl ">{stat.count}</h1>
         </div>
       ))}
     </div>
