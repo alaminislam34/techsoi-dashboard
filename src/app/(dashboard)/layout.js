61c99fb00d41f4e2fed6863aa8ad1c15b1d5cf88
alaminislam34 <mi3548514@gmail.com>
2026-01-02 02:58:45 +0600

    added some changes

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -16,12 +16,12 @@ export default function DashboardLayout({ children }) {
         {/* Main Content Area */}
         <section className="flex-1 flex flex-col min-w-0 h-screen relative z-10">
           {/* Sticky Header with Padding */}
-          <header className="sticky shadow top-0 z-50 p-4 xl:p-6 pb-2 ">
+          <header className="sticky shadow top-0 z-50 p-4 xl:p-6">
             <DashboardNavbar />
           </header>
 
           {/* Scrollable Body Content Area */}
-          <div className="flex-1 overflow-y-auto overflow-x-hidden p-4 xl:p-6 pt-0 space-y-6 scroll-smooth">
+          <div className="flex-1 overflow-y-auto overflow-x-hidden p-4 md:p-6 pt-0 space-y-6 scroll-smooth">
             {children}
           </div>
         </section>
